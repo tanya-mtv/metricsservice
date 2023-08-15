2eac0a407b11eff04c47095c8d0786012e63ab6e
tanya_mtv <tanya_mtv@list.ru>
2023-08-15 19:42:21 +0400

    repalace mistakes

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,17 +1,19 @@
 package utils
 
-import "strconv"
+import (
+	"strconv"
 
+	"github.com/tanya-mtv/metricsservice/internal/repository"
+)
 
-
-func CounterToBytes(number Counter) []byte {
+func CounterToBytes(number repository.Counter) []byte {
 
 	b := []byte(strconv.FormatInt(int64(number), 10))
 
 	return b
 }
 
-func GaugeToBytes(number Gauge) []byte {
+func GaugeToBytes(number repository.Gauge) []byte {
 
 	b := []byte(strconv.FormatFloat(float64(number), 'f', -1, 64))
 

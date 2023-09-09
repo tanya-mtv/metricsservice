28ecac96b27da52e762f43d36830dd0af42b22f6
tanya_mtv <tanya_mtv@list.ru>
2023-09-09 21:14:24 +0400

    Set Accept-Encoding header

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -111,9 +111,9 @@ func (h *Handler) PostMetricsList(c *gin.Context) {
 		return
 	}
 	fmt.Printf("Response PostMetricsList %+v \n", string(data))
-	c.Writer.Header().Set("Content-Type", "application/json")
-	c.Writer.Header().Set("Accept-Encoding", "gzip")
-
+	// c.Writer.Header().Set("Content-Type", "application/json")
+	// c.Writer.Header().Set("Accept-Encoding", "gzip")
+	c.Writer.Header().Set("Content-Type", "application/json; charset=utf-8")
 	c.JSON(http.StatusOK, list)
 
 }

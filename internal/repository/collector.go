beec18c6dee55ab0539ba98f20472c3a3fa9525e
tanya_mtv <tanya_mtv@list.ru>
2023-09-15 21:18:17 +0400

    my debug without gzip in metriclist

--- internal/repository/collector.go
+++ internal/repository/collector.go
@@ -69,7 +69,7 @@ func (m *MetricRepositoryCollector) GetAllMetricsList() []models.Metrics {
 		k += 1
 		tmp := float64(value)
 		listmetrics = append(listmetrics, models.Metrics{ID: name, MType: "gauge", Value: &tmp})
-		if k == 1 {
+		if k == 3 {
 			break
 		}
 

72f9c0b510ebef3c2a4262c3d2f506873469cdba
tanya_mtv <tanya_mtv@list.ru>
2023-09-28 21:15:24 +0400

    15 increment is completed

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -46,51 +46,62 @@ func (a *agent) Run() error {
 		select {
 		case <-ctx.Done():
 			stop()
+
 			return nil
 		case <-pollIntervalTicker.C:
 			go a.metrics.MetricsMonitor()
 			go a.metrics.MetricsMonitorGopsutil(ctx)
 		case <-reportIntervalTicker.C:
-			metrics := a.metrics.GetAllMetricList()
-			numjobs := len(metrics)
-			jobs := make(chan models.Metrics, numjobs)
-
-			// создаем буферизованный канал для отправки результатов
-			results := make(chan models.Metrics, numjobs)
-			// создаем и запускаем 3 воркера, это и есть пул,
-			// передаем id, это для наглядности, канал задач и канал результатов
-			for w := 1; w <= a.cfg.RateLimit; w++ {
-				go worker(w, jobs, results)
-			}
-
-			// отправляем в канал задач какие-то данные
-			// задач у нас 5, а воркера 3, значит одновременно решается только 3 задачи
-			for j := 1; j <= numjobs; j++ {
-				jobs <- metrics[j-1]
-			}
-			// закрываем канал на стороне отправителя
-			close(jobs)
-
-			// забираем из канала результатов результаты ;)
-			for b := 1; b <= numjobs; b++ {
-				go a.metrics.PostMessage(ctx, metrics[b-1])
-				<-results
-			}
+			go a.createWorkerPool(ctx)
 		}
-
 	}
 
 }
 
-func worker(id int, jobs <-chan models.Metrics, results chan<- models.Metrics) {
+func (a *agent) createWorkerPool(ctx context.Context) {
+	metrics := a.metrics.GetAllMetricList()
+	numjobs := len(metrics)
+	jobs := make(chan models.Metrics, numjobs)
+
+	// создаем буферизованный канал для отправки результатов
+	results := make(chan models.Metrics, numjobs)
+
+	// создаем и запускаем 3 воркера, это и есть пул,
+	// передаем id, это для наглядности, канал задач и канал результатов
+	for w := 1; w <= a.cfg.RateLimit; w++ {
+		go worker(jobs, results)
+	}
+
+	// отправляем в канал задач метрики
+	// задач у нас 5, а воркера 3, значит одновременно решается только 3 задачи
+	for j := 1; j <= numjobs; j++ {
+		fmt.Println("get metric  ", metrics[j-1])
+		jobs <- metrics[j-1]
+	}
+	// закрываем канал на стороне отправителя
+	close(jobs)
+
+	go a.recieveChainData(ctx, results)
+}
+func (a *agent) recieveChainData(ctx context.Context, res chan models.Metrics) {
+	defer close(res)
+	for {
+		val, ok := <-res
+		if !ok {
+			fmt.Println(val, "<-- loop broke!")
+			break
+		} else {
+			a.metrics.PostMessage(ctx, val)
+
+		}
+	}
+
+}
+
+func worker(jobs <-chan models.Metrics, results chan<- models.Metrics) {
 	for val := range jobs {
-		// для наглядности будем выводить какой рабочий начал работу и кго задачу
-		fmt.Println("рабочий", id, "запущен задача", val)
 		// немного замедлим выполнение рабочего
-		time.Sleep(3 * time.Second)
-		// для наглядности выводим какой рабочий завершил какую задачу
-		fmt.Println("рабочий", id, "закончил задача", val)
-		// отправляем результат в канал результатов
+		time.Sleep(1 * time.Second)
 		results <- val
 	}
 }
